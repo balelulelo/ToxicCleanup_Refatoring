3e8c690cf9aba8d7bac0c9be39576a2caaf0800f
jaesekii <162032031+balelulelo@users.noreply.github.com>
2026-05-11 18:20:12 +1000

    refactored WeatherSpawnPoint class to remove the usage of an ArrayList

--- src/toxiccleanup/builder/weather/WeatherSpawnPoint.java
+++ src/toxiccleanup/builder/weather/WeatherSpawnPoint.java
@@ -7,10 +7,9 @@ import toxiccleanup.engine.timing.TickTimer;
 import toxiccleanup.builder.GameState;
 import toxiccleanup.builder.Tickable;
 
-import java.util.ArrayList;
-
 /**
- * A WeatherSpawnPoint is responsible for managing timed creation of preset weather Phenomena.
+ * A WeatherSpawnPoint is responsible for managing timed creation of weather Phenomena at a fixed position on
+ * the map.
  * It takes a given {@link Positionable} position, everytime the given {@link TickTimer} finishes
  * it will call the given {@link Spawner} lambda
  */
@@ -18,35 +17,44 @@ public class WeatherSpawnPoint implements Tickable {
 
     private final TickTimer timer;
     private final Spawner spawner;
-    private final ArrayList<Positionable> positions = new ArrayList<>();
+    private final Positionable position;
 
     /**
      * Constructs a new instance of {@link WeatherSpawnPoint}
      * using the given position, timer and spawner.
      *
-     * @param position - position that will be passed to the {@link Spawner} when it fires.
-     * @param timer    - the timer that will be used to determine when to call the {@link Spawner}.
-     * @param spawner  - the {@link Spawner} we intend to call whenever the {@link TickTimer}
+     * <p>Precondition: {@code position}, {@code timer}, and {@code spawner} must not be NULL</p>
+     *
+     * @param position position that will be passed to the {@link Spawner} when it fires.
+     * @param timer    the timer that will be used to determine when to call the {@link Spawner}.
+     * @param spawner  the {@link Spawner} we intend to call whenever the {@link TickTimer}
      *                 is finished.
      */
     public WeatherSpawnPoint(Positionable position, TickTimer timer, Spawner spawner) {
-        this.positions.add(position);
+        this.position = position;
         this.timer = timer;
         this.spawner = spawner;
     }
 
 
     /**
+     * Returns a copy of this spawn point's fixed position
+     *
+     * <p>the defensive copy is still implemented there to prevent external modification
+     *  of the internal position</p>
+     *
      * @return the correctly stored position of the {@link WeatherSpawnPoint} in question.
      */
     public Positionable getPosition() {
-        Positionable position = positions.getFirst();
         return new Position(position.getX(), position.getY());
     }
 
 
     /**
      * Advances component state by one game tick using engine and game context.
+     *
+     * <p>Ticks the internal timer. If timer has finished, it will create a new weather event at the
+     * given spawn point's position and adds it to the weather system</p>
      *
      * @param state The state of the engine, including the mouse, keyboard information and
      *              dimension. Useful for processing keyboard presses or mouse movement.
