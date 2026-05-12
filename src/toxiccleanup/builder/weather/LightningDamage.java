0ca0d6b4cc469a5f28ac3db63ce93f4e3c1074dd
jaesekii <162032031+balelulelo@users.noreply.github.com>
2026-05-12 13:14:57 +1000

    removed variable x and y in lightning damage since it's unused

--- src/toxiccleanup/builder/weather/LightningDamage.java
+++ src/toxiccleanup/builder/weather/LightningDamage.java
@@ -7,8 +7,6 @@ import toxiccleanup.builder.Damage;
  * Lightning Type Damage, can not deal damage to lightning rods specifically.
  */
 public class LightningDamage extends Damage {
-    private int x = 0;
-    private int y = 0;
     public static final String TYPE = "lightning";
 
     public LightningDamage(Positionable position) {
